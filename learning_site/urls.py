e30b03098548d20e7884ffe93d2da74e7985c917
naivedyabansal <naivedyabansal@gmail.com>
2017-01-22 17:33:30 +0530

    feat: add inheritance to home page templates add static css files ti project using python in debug mode.

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -22,3 +23,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
