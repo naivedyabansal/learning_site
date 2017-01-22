4f053e50c380f316ae8b18cf56217e81651d5dc8
naivedyabansal <naivedyabansal@gmail.com>
2017-01-22 19:49:05 +0530

    feat: add page to view steps of a particular course make requisite view and add url path

--- courses/views.py
+++ courses/views.py
@@ -7,3 +7,8 @@ def course_list(request):
     # output = ', '.join([str(course) for course in courses])
     # return HttpResponse(output)
     return render(request, 'courses/course_list.html', {'courses':courses})
+
+
+def course_detail(request, pk):
+    course = Course.objects.get(pk=pk)
+    return render(request, 'courses/course_detail.html', {'course': course})
