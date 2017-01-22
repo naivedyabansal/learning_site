80cc31395735f7a7007a43aad1cd8425ccad2b3f
naivedyabansal <naivedyabansal@gmail.com>
2017-01-22 16:22:19 +0530

    feat: add Model Course to django admin add template to display courses list

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
+
     def __str__(self):
         return self.title
+
 # Create your models here.
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Course
 
 def course_list(request):
     courses = Course.objects.all()
-    output = ', '.join([str(course) for course in courses])
-    return HttpResponse(output)
-# Create your views here.
+    # output = ', '.join([str(course) for course in courses])
+    # return HttpResponse(output)
+    return render(request, 'courses/course_list.html', {'courses':courses})
