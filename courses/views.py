b11ce72fd1698c2ccd6837c6164d0c113a4fdc5f
naivedyabansal <naivedyabansal@gmail.com>
2017-01-23 15:56:27 +0530

    feat: add HTML file to view a particular step of a course.
    url specified with step's primary key and course primary key which
    must match the step's foreign key.
    pointing to corresponding course

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
-from .models import Course
+from .models import Course, Step
 
 def course_list(request):
     courses = Course.objects.all()
@@ -10,5 +10,10 @@ def course_list(request):
 
 
 def course_detail(request, pk):
-    course = Course.objects.get(pk=pk)
+    # course = Course.objects.get(pk=pk)
+    course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': course})
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step': step})
